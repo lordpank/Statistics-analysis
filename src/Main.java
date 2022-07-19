5f258373595aa5a44bd43a999fce75030e065acc
lordpank <lordpank@gmail.com>
2022-07-19 20:02:19 +0300

    Material added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {254, 804, 107, 896};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
+    }
+}
